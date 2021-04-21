17af12a35092f2d4a135cb31f7ea1405d2ab82ed
Rajsekhar <grsrreddy9@gmail.com>
2021-04-21 00:41:22 -0400

    created all forms and need to check equipment form code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {CreateEquipmentPage, CreateDepartmentPage} from './pages';
+import {CreateEquipmentPage, CreateDepartmentPage, CreateProductPage} from './pages';
 function App() {
   return (
     <div className="App">
